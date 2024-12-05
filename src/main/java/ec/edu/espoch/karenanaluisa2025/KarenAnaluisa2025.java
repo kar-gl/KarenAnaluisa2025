f3a1d9096f86d0c0e92f681ccd39ca2e293c03f7
JIrleidySantos <jirleidysantosm@gmail.com>
2024-12-05 13:52:40 -0500

    inicio

---
+++ src/main/java/ec/edu/espoch/karenanaluisa2025/KarenAnaluisa2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.karenanaluisa2025;
+
+/**
+ *
+ * @author SO-LAB1-PC23
+ */
+public class KarenAnaluisa2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
